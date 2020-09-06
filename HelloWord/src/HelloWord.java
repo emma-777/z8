fe007c07662cf389e576f5668ad949648d054af5
Emma <an.vital@mail.ru>
2020-09-06 10:48:49 +0400

    <kf ,kf
    бла бла

---
+++ HelloWord/src/HelloWord.java
@@ -0,0 +1,15 @@
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.out.println("Hello Word!");
+System.out.println("Hello Word!");
+System.out.println("Hello Word!");
+System.out.println("Hello Word!");
+System.out.println("Hello Word!");
+System.out.println("Hello all!");
+	}
+
+}
